ea6b2e4ee3ea441883b2a1414a5bc198985e7d02
JanaDuraisamy <145962869+JanaDuraisamy@users.noreply.github.com>
2023-09-25 12:15:05 +0530

    Add files via upload

---
+++ multi inheritance.py
@@ -0,0 +1,34 @@
+# class P1:
+#     def m1(self):
+#         print("Parent 1")
+# class P2:
+#     def m2(self):
+#         print("Parent 2")
+# class C(P1,P2):
+#     def m3(self):
+#         print("child class")
+#
+# c=C()
+# c.m1()
+# c.m2()
+# c.m3()
+
+
+
+# --METHOD RESOLUTION ORDER--(MRO)
+class P1:
+    def m1(self):
+        print("Parent 1")
+class P2:
+    def m1(self):
+        print("Parent 2")
+class C(P1,P2):
+    def m2(self):
+        print("child class")
+
+c=C()
+c.m1()
+c.m2()
+
+
+
---
+++ suriya.py
@@ -0,0 +1,87 @@
+# s=("jana,vicky,karthi,suriya")
+# print(s.upper())
+# print(s.lower())
+# print(len(s))
+# print(s.capitalize())
+# print(s.replace("karthi","kai"))
+
+s="learning good"
+i=0
+output=""
+while i<=0:
+    output=output+s[::-1]
+    i=i+1
+print(output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
